6a3c1db18cb44bdad1988cc0bb65cca2d3bad549
Pratik Thapa <pratik.philos@live.com>
2024-02-22 21:55:19 -0500

    parsing work

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 )
 
-func main() {
+func parsingEnv(filePath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// skip new line and comments
+		if len(line) == 0 || strings.HasPrefix(line, "#") {
+			continue
+		}
+		// remove the empty lines
+		splitStr := strings.Split(line, "\n")
+		fmt.Println(splitStr)
+		// read each value on = strings.splitAfter =
+	}
+	// create a map so that when user calls it with {{key}} the value is returned
+	return nil
+}
+
+func printJson() {
 	chunk := map[string]interface{}{
 		"code":  "5000",
 		"error": "Error",
@@ -14,4 +40,12 @@ func main() {
 	}
 	val, _ := json.Marshal(chunk)
 	fmt.Println(string(val))
+}
+
+func main() {
+	printJson()
+	err := parsingEnv("./.env.global")
+	if err != nil {
+		panic(err)
+	}
 }
