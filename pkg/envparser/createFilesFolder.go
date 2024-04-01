57b4635392d84609906bd5c0876a4d7dde5cf9f2
Pratik Thapa <pratik.philos@live.com>
2024-03-31 21:09:57 -0400

    minor changes

--- pkg/envparser/createFilesFolder.go
+++ pkg/envparser/createFilesFolder.go
@@ -19,18 +19,18 @@ func CreateDefaultEnvs(envName *string) error {
 	if err != nil {
 		return err
 	}
-	// create a env folder in the root of the project
+
+	// create an env folder in the root of the project
 	envDirpath := filepath.Join(projectRoot, "env")
 	envFilePath := filepath.Join(envDirpath, defaultEnv+".env") // global.env as the default
 	if _, err := os.Stat(envDirpath); os.IsNotExist(err) {
-		fmt.Println("Creating env directory...")
+		fmt.Println("Created env directory \u2713")
 		if err := os.Mkdir(envDirpath, 0755); err != nil {
 			fmt.Println("Error creating env directory")
 			return err
 		}
 	}
 	if _, err := os.Stat(envFilePath); os.IsNotExist(err) {
-		fmt.Println("Creating global.env for your secrets...")
 		file, err := os.Create(envFilePath)
 		if err != nil {
 			fmt.Println("Error creating global environment \u2717")
