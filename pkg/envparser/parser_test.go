278c2ecef5ffac46712f808d86b4608382dbd504
Pratik Thapa <pratik.philos@live.com>
2024-05-07 17:16:22 +0900

    adding test for trimmed quotes

--- pkg/envparser/parser_test.go
+++ pkg/envparser/parser_test.go
@@ -4,6 +4,29 @@ import (
 	"testing"
 )
 
+func TestTrimQuotes(t *testing.T) {
+	// it's a bit tricky to test since trim quotes trims the quotest the value from env file
+	testCases := []struct {
+		input  string
+		output string
+	}{
+		{input: "", output: ""},
+		{input: "test's value", output: "test's value"},
+		{input: "userNam2", output: "userNam2"},
+	}
+
+	for _, tc := range testCases {
+		resultStr := trimQuotes(tc.input)
+		if resultStr != tc.output {
+			t.Errorf(
+				"Expected output does not match the result: \n%v \nvs \n%v",
+				tc.output,
+				resultStr,
+			)
+		}
+	}
+}
+
 func TestSetEnvironment(t *testing.T) {
 	/* Test cases
 	* provided flag is captured.
