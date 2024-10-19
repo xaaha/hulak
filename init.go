c2d0c7b8f43c3fcf37d11e9243107c835e1e7bb4
Pratik Thapa <pratik.philos@live.com>
2024-10-19 10:38:40 -0400

    make main.go a playground for now
    where I can test simple stuff

--- init.go
+++ init.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
 	"github.com/xaaha/hulak/pkg/envparser"
 )
 
@@ -13,4 +17,30 @@ func InitializeProject() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+/*
+Initialize the project and how to substiture a variable.
+This function is just for my dumb brain that forgets how to do simple stuff in a program I wrote
+*/
+func testInitialization() {
+	InitializeProject()
+
+	envMap, err := envparser.GenerateSecretsMap()
+	if err != nil {
+		panic(err)
+	}
+
+	// print entire json
+	niceJson, _ := json.MarshalIndent(envMap, "", "  ")
+	fmt.Println(string(niceJson))
+
+	// how to substitute variable
+	finalAns, err := envparser.SubstitueVariables("env{{PORT}}", envMap)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(finalAns)
+
+	fmt.Println("Default Environment value:", os.Getenv("hulakEnv"))
 }
--- main.go
+++ main.go
@@ -1,38 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
-
-	"github.com/xaaha/hulak/pkg/envparser"
 	ymlReader "github.com/xaaha/hulak/pkg/hulak_yaml_reader"
 )
 
-// Initialize the project and how to substiture a variable
-func testInitialization() {
-	InitializeProject()
-
-	envMap, err := envparser.GenerateSecretsMap()
-	if err != nil {
-		panic(err)
-	}
-
-	// print entire json
-	niceJson, _ := json.MarshalIndent(envMap, "", "  ")
-	fmt.Println(string(niceJson))
-
-	// how to substitute variable
-	finalAns, err := envparser.SubstitueVariables("env{{PORT}}", envMap)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(finalAns)
-
-	fmt.Println("Default Environment value:", os.Getenv("hulakEnv"))
-}
-
 func main() {
-	testInitialization()
+	// testInitialization()
 	ymlReader.ReadingYamlWithStruct()
 }
