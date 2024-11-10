8e8343a6b540deff8e9bed4c3e01938bc6b9d82f
Pratik Thapa <pratik.philos@yahoo.com>
2024-11-09 19:02:09 -0500

    test cases

--- pkg/yamlParser/reader_test.go
+++ pkg/yamlParser/reader_test.go
@@ -1,7 +1,38 @@
 package yamlParser
 
-// func TestReadingYamlWithStruc(t *testing.T) {
-//   yamlFiles = []struct{
-//
-//   }{}
-// }
+import (
+	"fmt"
+	"testing"
+)
+
+func TestReadingYamlWithStruc(t *testing.T) {
+	content := `
+  ---
+method: post
+url: https://graphql.postman-echo.com/graphql
+urlparams:
+  foo: bar
+  baz: bin
+headers:
+  Content-type: application/json
+body:
+  # formdata:
+  #   foo: bar
+  #   baz: bin
+  randomThing:
+    foo: this is being ignored correctly
+  # this should be invalid or just ignored
+  graphql:
+    query: |
+      query Hello {
+        hello(person: { name: "pratik", age: 11 })
+      }
+  # variables:
+  #   run: true
+  `
+	// pos is invalid
+	// post and POST is valid
+	// error when url, body, and method is invalid
+	// may be check if the url, method is also missing
+	fmt.Println(content)
+}
