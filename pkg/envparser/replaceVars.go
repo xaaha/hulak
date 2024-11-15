ec55d0b8ef3f89253b7c718bd5ca82561b7be9d5
pratik thapa <pratikthapa@ml2xvyvxp7.mynetworksettings.com>
2024-11-15 17:43:51 -0500

    trying to use go tempalte instead of custom recursive function to replace the EnvVars

--- pkg/envparser/replaceVars.go
+++ pkg/envparser/replaceVars.go
@@ -37,3 +37,47 @@ func SubstituteVariables(strToChange string, mapWithVars map[string]string) (str
 	}
 	return strToChange, nil
 }
+
+//
+// func SubstituteVariables(strToChange string, mapWithVars map[string]string) (string, error) {
+// 	if len(strToChange) == 0 {
+// 		return "", errors.New("Input string is empty")
+// 	}
+// 	tmpl, err := template.New("template").Parse(strToChange)
+// 	if err != nil {
+// 		return "", fmt.Errorf("Failed to parse template: %w", err)
+// 	}
+// 	var result bytes.Buffer
+// 	err = tmpl.Execute(&result, mapWithVars)
+// 	if err != nil {
+// 		return "", fmt.Errorf("Failed to execute template: %w", err)
+// 	}
+// 	return result.String(), nil
+// }
+//
+// func main() {
+// 	str := "Hello, {{.name}}! Welcome to {{.place}}. This is a {{.project}}. And a comment {{/* a comment */}}. Finally, an {{.error}}"
+// 	vars := map[string]string{
+// 		"name":    "John",
+// 		"place":   "Gopherland",
+// 		"project": "{{.hulak}}",
+// 		"hulak":   "Hulak v1",
+// 	}
+// 	// in case the map has nested vals
+// 	for key, val := range vars {
+// 		result, err := SubstituteVariables(val, vars)
+// 		vars[key] = result
+// 		if err != nil {
+// 			fmt.Println("Error:", err)
+// 		} else {
+// 			fmt.Println(result)
+// 		}
+// 	}
+//
+// 	result, err := SubstituteVariables(str, vars)
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 	} else {
+// 		fmt.Println(result)
+// 	}
+// }
