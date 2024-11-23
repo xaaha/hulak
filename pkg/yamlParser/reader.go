57f5cbe22b149d422dc04e00e53c0524459862a5
Pratik Thapa <pratik.philos@yahoo.com>
2024-11-22 20:51:10 -0500

    cleaned up the function

--- pkg/yamlParser/reader.go
+++ pkg/yamlParser/reader.go
@@ -137,8 +137,7 @@ func ReadYamlForHttpRequest(filePath string, secretsMap map[string]string) strin
 	// check if body is valid
 	// TODO: 2. Once the 1st todo is done, remove this non nil check. This sould exist in call.go
 	// if the body is not present in the body, then the body is nil
-	if user.Body == nil {
-	} else if !user.Body.IsValid() {
+	if user.Body != nil && !user.Body.IsValid() {
 		utils.PanicRedAndExit(
 			"Invalid Body. Make sure body contains only one valid argument.\n %v",
 			user.Body,
