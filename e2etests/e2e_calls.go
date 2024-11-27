c2e20eb1f6db5755e50b88956d61fb2e20a4ce58
Pratik Thapa <pratik.philos@yahoo.com>
2024-11-26 20:49:34 -0500

    final e2e test calls

---
+++ e2etests/e2e_calls.go
@@ -0,0 +1,49 @@
+package e2etests
+
+import (
+	"fmt"
+
+	apicalls "github.com/xaaha/hulak/pkg/apiCalls"
+	fileReader "github.com/xaaha/hulak/pkg/yamlParser"
+)
+
+func RunFormData(envMap map[string]string) {
+	formDataJSONString := fileReader.ReadYamlForHttpRequest(
+		"test_collection/form_data.yaml",
+		envMap,
+	)
+	apiInfo, err := apicalls.CombineAndCall(formDataJSONString)
+	if err != nil {
+		_ = fmt.Errorf("Error occoured in main %v", err)
+	}
+	resp := apicalls.StandardCall(apiInfo)
+	fmt.Println(resp)
+}
+
+// runs actual form
+func RunFormDataError(envMap map[string]string) {
+	formDataJSONString := fileReader.ReadYamlForHttpRequest(
+		"test_collection/form_data_error.yml",
+		envMap,
+	)
+	apiInfo, err := apicalls.CombineAndCall(formDataJSONString)
+	if err != nil {
+		_ = fmt.Errorf("Error occoured in main %v", err)
+	}
+	resp := apicalls.StandardCall(apiInfo)
+	fmt.Println(resp)
+}
+
+// runs actual call from "test_collection/url_encoded_form.yaml.yaml"
+func RunUrlEncodedFormData(envMap map[string]string) {
+	formDataJSONString := fileReader.ReadYamlForHttpRequest(
+		"test_collection/url_encoded_form.yaml",
+		envMap,
+	)
+	apiInfo, err := apicalls.CombineAndCall(formDataJSONString)
+	if err != nil {
+		_ = fmt.Errorf("Error occoured in main %v", err)
+	}
+	resp := apicalls.StandardCall(apiInfo)
+	fmt.Println(resp)
+}
--- main.go
+++ main.go
@@ -1,20 +1,11 @@
 package main
 
-import (
-	"fmt"
-
-	apicalls "github.com/xaaha/hulak/pkg/apiCalls"
-	fileReader "github.com/xaaha/hulak/pkg/yamlParser"
-)
+import "github.com/xaaha/hulak/e2etests"
 
 func main() {
 	envMap := InitializeProject()
 	// apicalls.TestApiCalls() // temp call.. replace with mock
-	jsonString := fileReader.ReadYamlForHttpRequest("test_collection/form_data.yaml", envMap)
-	apiInfo, err := apicalls.CombineAndCall(jsonString)
-	if err != nil {
-		_ = fmt.Errorf("Error occoured in main %v", err)
-	}
-	resp := apicalls.StandardCall(apiInfo)
-	fmt.Println(resp)
+	e2etests.RunFormData(envMap)
+	e2etests.RunUrlEncodedFormData(envMap)
+	e2etests.RunFormDataError(envMap)
 }
