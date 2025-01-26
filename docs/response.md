8bfac8e6cab3500ab87028b1eff8a7fa21a648e6
Pratik Thapa <pratik.philos@yahoo.com>
2025-01-26 13:22:44 -0500

    readme and other documentation changes

--- docs/response.md
+++ docs/response.md
@@ -1,9 +1,10 @@
 # Response
 
 - Std output is always a json response. If the response body is a format other than json, it would be converted to string.
-- But, appropriate response format is saved in the same path as the calling file
-- By default rsponse is saved with the same name with "\_response" added. So, test.yaml would have `test_response.json`
-- If the reponse is plain text it would be `test_response.txt
+- But, appropriate response format is saved in the same path as the file making the call.
+- By default rsponse is saved with the same name with "\_response.json" added.
+  So, `test.yaml` would have it's response saved as`test_response.json`
+- If the reponse is plain text it would be `test_response.txt`
 - If the response is xml, the response file would have the proper suffix `test_response.xml`
 - If the reponse is not successful, the entire std output is saved as a json file
 
--- main.go
+++ main.go
@@ -25,22 +25,3 @@ func main() {
 
 	RunTasks(filePathList, envMap)
 }
-
-/*
-- How do we handle making a file dependent on a response of different request call?
-  - Save the file to it's respective file type concurrently
-  - Once the file is done saving, lock it and copy the content in a buffer (mutex)
-  - Then release the content
-  - Then use the content to recursively find the key mentioned in the template {{getValueOf accesstoken employee_auth}}
-    - This is where I need to make the change pkg/envparser/replaceVars.go
-    - function name, value you want to grab, and file name you want to grab from.
-    - File name should be unique. Or Provide file path. If there are multiple paths found, use the first one
-    -
-  - Also, need to think about how to handle a situation where the call you are depending on is dependent on another call and they are all concurrently running
-     - Do we not use the concurrency at that time?
-     - For example, {{getValueOf user_uuid employeeDetails}}
-     - But, employeeDetails file has {{getValueOf foo bar}} and they are all running concurrently. Do we run one after the other in that case?
-     - since we are calling all the files concurrently, we need to lock a json file with mutex, and then check if other file is making the call.
-      Last thing we need is to multiple calls calling the same AuthCall
-     - Use channel to communicate the response before writing the file?
-*/
