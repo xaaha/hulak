b8ffc4d55743fb03b329466096bd990cc8154bc1
Pratik Thapa <pratik.philos@yahoo.com>
2025-02-09 20:13:54 -0500

    add interface for apiConfigProvider....

--- pkg/apiCalls/types.go
+++ pkg/apiCalls/types.go
@@ -1,7 +1,14 @@
 package apicalls
 
+import "github.com/xaaha/hulak/pkg/yamlParser"
+
 // structure of the result to print in the console as the std output
 type CustomResponse struct {
 	Body           interface{} `json:"Body"`
 	ResponseStatus string      `json:"Response Status"`
+}
+
+// Implements GetApiConfig function that returns ApiInfo struct
+type ApiInfoProvider interface {
+	GetApiConfig(secretsMap map[string]interface{}, path string) (yamlParser.ApiInfo, error)
 }
