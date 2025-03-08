1fee91a495a5d746c63ca4c0ae5de10395857110
Pratik Thapa <pratik.philos@yahoo.com>
2025-03-07 20:53:23 -0500

    environmet migration from postman

--- pkg/migration/postman.go
+++ pkg/migration/postman.go
@@ -10,20 +10,6 @@ import (
 	"github.com/xaaha/hulak/pkg/utils"
 )
 
-// hulak migrate "./globals.json"
-// if the json has info object it's collection
-// if the json has "name": "Globals",and  "_postman_variable_scope": "globals", then it's the env
-// If name is empty ""  or name == "globals" then migrate things to global
-// Otherwise a name in pm json file should create a new env file with the exact name if the env file does not exists
-// If it's globals and  _postman_variable_scope has globals scope on it, then push it to the globals then push this into global.env
-// If the name in json exists in the env folder there is no need to create it, just migrate
-// Existing function to create folder and file for the environment
-// Otherwise just create then same environment as the name
-// values array struct
-// Collection:
-// for collection: if the collection has variables and the variables is coming from the variable below add it to the globals
-// but what if there is already a variable that exists with the same name in global? Because,
-
 type KeyValuePair struct {
 	Key   string `json:"key"`
 	Value string `json:"value"`
@@ -79,7 +65,7 @@ type Collection struct {
 }
 
 // Reads the json file and returns the jsonString
-func ReadPmFile(filePath string) map[string]any {
+func readPmFile(filePath string) map[string]any {
 	var jsonStrFile map[string]any
 	jsonByteVal, err := os.ReadFile(filePath)
 	if err != nil {
@@ -96,11 +82,62 @@ func ReadPmFile(filePath string) map[string]any {
 	return jsonStrFile
 }
 
-// migrates a postman json environment file to `key = value` pair
-// inside the env dir
-func MigrateEnv(env Environment) {
+// takes the jsonString  and convets to Environment struct
+func prepareEnvStruct(jsonStr map[string]any) (Environment, error) {
+	var env Environment
+
+	// extract name first
+	if name, ok := jsonStr["name"].(string); ok {
+		env.Name = name
+	} else {
+		return env, utils.ColorError("name field is missing or not a string")
+	}
+
+	if values, ok := jsonStr["values"].([]any); ok {
+		for _, v := range values {
+			if valueMap, ok := v.(map[string]any); ok {
+				var envValue EnvValues
+
+				if key, ok := valueMap["key"].(string); ok {
+					envValue.Key = key
+				} else {
+					return env, utils.ColorError("key field is missing or not a string in EnvValues")
+				}
+
+				if value, ok := valueMap["value"].(string); ok {
+					envValue.Value = value
+				} else {
+					return env, utils.ColorError("value field is missing or not a string in EnvValues")
+				}
+
+				if enabled, ok := valueMap["enabled"].(bool); ok {
+					envValue.Enabled = enabled
+				} else {
+					return env, utils.ColorError("enabled field is missing or not a boolean in EnvValues")
+				}
+
+				env.Values = append(env.Values, envValue)
+			} else {
+				return env, utils.ColorError("value is not a valid map for EnvValues")
+			}
+		}
+	} else {
+		return env, utils.ColorError("values field is missing or not an array")
+	}
+
+	if scope, ok := jsonStr["_postman_variable_scope"].(string); ok {
+		env.Scope = scope
+	} else {
+		return env, utils.ColorError("scope field is missing or not a string")
+	}
+
+	return env, nil
+}
+
+// migrates a postman json environment file to `key = value` pair inside the env dir
+func migrateEnv(env Environment) {
 	var message strings.Builder
-	message.WriteString("### Postman Env Migration ###\n")
+	message.WriteString("\n### Postman Env Migration ###\n")
 	for _, eachVarItem := range env.Values {
 		keyVal := fmt.Sprintf("%s = %s\n", eachVarItem.Key, eachVarItem.Value)
 		if !eachVarItem.Enabled {
@@ -110,12 +147,13 @@ func MigrateEnv(env Environment) {
 	}
 	content := message.String()
 
-	byteSlice := []byte(content)
-
-	// env name,
+	// env fileName
 	var envFileName string
-	if env.Name == "" || env.Name == "globals" {
+	lowerCased := strings.ToLower(env.Name)
+	if env.Name == "" || lowerCased == "globals" {
 		envFileName = utils.DefaultEnvVal
+	} else {
+		envFileName = env.Name
 	}
 
 	filePath, err := envparser.CreateEnvDirAndFiles(envFileName)
@@ -124,6 +162,7 @@ func MigrateEnv(env Environment) {
 		return
 	}
 
+	// append the content
 	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		utils.PrintRed("Error opening or creating file: " + err.Error())
@@ -137,34 +176,32 @@ func MigrateEnv(env Environment) {
 		return
 	}
 
-	utils.PrintGreen("Environment migration successful!")
-
-	// TODO-1:
-	// either create the file or append to an existing one.
-	// global is mandatory. Create proper env/global.env (at least, if it does not exist)
-	// grab name from the json (env.Name)
-	// use CreateEnvDirAndFiles. Create globals or any other env file
-	// then append the content above to the appropriate file
-	// if the env folder exists, but it does not have the file "name" (Globals is global), create the file and add the content
-	// if the file exists, append the content at the end of the file (add comment)
-
-	// working on: Test and ensure it works
+	utils.PrintGreen("\nEnvironment migration successful!")
 }
 
 func MigrateCollection(collection Collection) {
 }
 
 func CompleteMigration(filePath []string) {
-	// loop over the path array
-	// for each path, read the pm file with ReadPmFile function
-	// ReadPmFile function returns jsonString. Use the string, to find out what the json it is
-	// Then marshall the jsonString to the appropriate struct, either Environment or  Collection
-	// If the jsonString is envFile, migrateEnv
-	// If the jsonString is collection, migrateCollection
+	for _, path := range filePath {
+		jsonStr := readPmFile(path)
+
+		if isEnv(jsonStr) {
+			env, err := prepareEnvStruct(jsonStr)
+			if err != nil {
+				fmt.Println("error occurred while converting to Environment:", err)
+				return
+			}
+			migrateEnv(env)
+		}
+		if isCollection(jsonStr) {
+			fmt.Println("Collection Migration Coming Soon")
+		}
+	}
 }
 
 // returns true if, the jsonString has "values" and "_postman_variable_scope"
-func IsEnv(jsonString map[string]any) bool {
+func isEnv(jsonString map[string]any) bool {
 	_, valuesExists := jsonString["values"]
 	_, pmVarScopeExists := jsonString["_postman_variable_scope"]
 
@@ -172,7 +209,7 @@ func IsEnv(jsonString map[string]any) bool {
 }
 
 // returns true, if the jsonString has "info" and "item"
-func IsCollection(jsonString map[string]any) bool {
+func isCollection(jsonString map[string]any) bool {
 	_, infoExists := jsonString["info"]
 	_, itemExists := jsonString["item"]
 
