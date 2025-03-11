369e2e04b42265a41f9d0871ae63dbac05fe9717
Pratik Thapa <pratik.philos@yahoo.com>
2025-03-10 21:31:50 -0400

    workig to add sub-commands

--- pkg/userFlags/subcommands.go
+++ pkg/userFlags/subcommands.go
@@ -4,25 +4,36 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	"github.com/xaaha/hulak/pkg/utils"
 )
 
 var migrate *flag.FlagSet
 
-// pmEnvFp *string
-
 // go's init func executes automatically, and registers the flags during package initialization
 func init() {
-	// Migrate to hulak currently only supports
-	// Postman's environment file or collection
 	migrate = flag.NewFlagSet("migrate", flag.ExitOnError)
-	// pmEnvFp = migrate.String("fp", "", "file path of the json env file from")
 }
 
-// hulak migrate "./globals.json"
-func MigratePostManEnv() {
+// Takes in filePath to a json file that needs migration.
+// Run as: hulak migrate "./filePath.json"
+func Migrate() {
+	if len(os.Args) < 2 {
+		utils.PrintWarning(
+			"Provide a subcommand or a flag. See docs at https://github.com/xaaha/hulak",
+		)
+		os.Exit(1)
+	}
+
+	// TODO-1: can't exit because, we expect a flag or a subcommand
+
 	switch os.Args[1] {
 	case "migrate":
-		tailArgs := migrate.Args()
-		fmt.Println("Tails", tailArgs[0])
+		_ = migrate.Parse(os.Args[2:])
+		fmt.Println("subcommand 'foo'")
+		fmt.Println("  tail:", migrate.Args())
+	default:
+		fmt.Println("expected 'foo' or 'bar' subcommands")
+		os.Exit(1)
 	}
 }
