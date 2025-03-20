365f029c05d61d81eb84dbb586f5a3df1fe5ee44
Pratik Thapa <pratik.philos@yahoo.com>
2025-03-19 21:13:36 -0400

    any not interface{}

--- pkg/yamlParser/reader.go
+++ pkg/yamlParser/reader.go
@@ -16,14 +16,14 @@ import (
 // Then, this function recursively replaces all variables {{.value}} specified in user's yaml values, with values from environment map
 // This is necessary, as the some variables, like URL needs correct string
 func replaceVarsWithValues(
-	dict map[string]interface{},
-	secretsMap map[string]interface{},
-) map[string]interface{} {
-	changedMap := make(map[string]interface{})
+	dict map[string]any,
+	secretsMap map[string]any,
+) map[string]any {
+	changedMap := make(map[string]any)
 
 	for key, val := range dict {
 		switch valTyped := val.(type) {
-		case map[string]interface{}:
+		case map[string]any:
 			changedMap[key] = replaceVarsWithValues(valTyped, secretsMap)
 		case string:
 			finalChangedValue, err := envparser.SubstituteVariables(valTyped, secretsMap)
@@ -36,7 +36,7 @@ func replaceVarsWithValues(
 				changedMap[key] = finalChangedValue
 			}
 		case map[string]string:
-			innerMap := make(map[string]interface{})
+			innerMap := make(map[string]any)
 			for k, v := range valTyped {
 				finalChangedValue, err := envparser.SubstituteVariables(v, secretsMap)
 				if err != nil {
@@ -57,7 +57,7 @@ func replaceVarsWithValues(
 }
 
 // Reads YAML, validates if the file exists, is not empty, and changes keys to lowercase
-func checkYamlFile(filepath string, secretsMap map[string]interface{}) (*bytes.Buffer, error) {
+func checkYamlFile(filepath string, secretsMap map[string]any) (*bytes.Buffer, error) {
 	if _, err := os.Stat(filepath); os.IsNotExist(err) {
 		utils.PanicRedAndExit("File does not exist, %s", filepath)
 	}
@@ -73,7 +73,7 @@ func checkYamlFile(filepath string, secretsMap map[string]interface{}) (*bytes.B
 		utils.PanicRedAndExit("Empty yaml file")
 	}
 
-	var data map[string]interface{}
+	var data map[string]any
 	dec := yaml.NewDecoder(file)
 	if err = dec.Decode(&data); err != nil {
 		utils.PanicRedAndExit("1. error decoding data: %v", err)
@@ -102,7 +102,7 @@ func checkYamlFile(filepath string, secretsMap map[string]interface{}) (*bytes.B
 }
 
 // checks the validity of all the fields in the yaml file meant for regular api call
-func FinalStructForApi(filePath string, secretsMap map[string]interface{}) User {
+func FinalStructForApi(filePath string, secretsMap map[string]any) User {
 	buf, err := checkYamlFile(filePath, secretsMap)
 	if err != nil {
 		utils.PanicRedAndExit("Error occured after reading yaml file: %v", err)
@@ -140,7 +140,7 @@ func FinalStructForApi(filePath string, secretsMap map[string]interface{}) User 
 // It returns AuthRequestBody struct
 func FinalStructForOAuth2(
 	filePath string,
-	secretsMap map[string]interface{},
+	secretsMap map[string]any,
 ) AuthRequestBody {
 	buf, err := checkYamlFile(filePath, secretsMap)
 	if err != nil {
