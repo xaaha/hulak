c852ab795befeefe4c4e1aa173b6eef5e16e3a58
xaaha <pratik.philos@yahoo.com>
2025-04-02 16:24:40 -0400

    use tabwriter instead of manually tabbing

--- pkg/userFlags/helper.go
+++ pkg/userFlags/helper.go
@@ -3,6 +3,8 @@ package userflags
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/xaaha/hulak/pkg/utils"
 )
@@ -43,20 +45,40 @@ func GenerateFilePathList(fileName string, fp string) ([]string, error) {
 // Helper function to print command usage
 func printHelp() {
 	utils.PrintWarning("Api Usage:")
-	fmt.Println("  hulak -env prod -f fileName                    - Find and run all 'fileName'")
-	fmt.Println("  hulak -env prod -fp path/tofile/getUser.yaml   - Run specific file")
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	writeCommandHelp(w, []*CommandHelp{
+		{"hulak -env prod -f fileName", "Find and run all 'fileName'"},
+		{"hulak -env prod -fp path/tofile/getUser.yaml", "Run specific file"},
+		{"hulak -env prod -fp path/tofile/getUser.yaml -debug", "Run in debug mode"},
+	})
+
+	w.Flush()
 
 	printHelpSubCommands()
 }
 
 // helper function to show valid subcommands
 func printHelpSubCommands() {
-	utils.PrintWarning("subcommand:")
-	fmt.Println(
-		"  hulak init                                     - Initializes default environment",
-	)
-	fmt.Println(
-		"  hulak init -env global prod test               - Initializes specific environments",
-	)
-	fmt.Println("  hulak migrate <file1> <file2> ...              - Migrates specified files")
+	utils.PrintWarning("Subcommands:")
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	writeCommandHelp(w, []*CommandHelp{
+		{"hulak init", "Initializes default environment"},
+		{"hulak init -env global prod test", "Initializes specific environments"},
+		{"hulak migrate <file1> <file2> ...", "Migrates specified files"},
+	})
+
+	w.Flush()
+}
+
+// CommandHelp holds a command and its description
+type CommandHelp struct {
+	Command     string
+	Description string
+}
+
+// writeCommandHelp writes commands and descriptions with proper alignment
+func writeCommandHelp(w *tabwriter.Writer, commands []*CommandHelp) {
+	for _, cmd := range commands {
+		fmt.Fprintf(w, "  %s\t- %s\n", cmd.Command, cmd.Description)
+	}
 }
