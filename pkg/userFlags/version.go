ef592a697e6cb848021f3028079f96ce79db00b4
Pratik Thapa <pratik.philos@yahoo.com>
2025-04-05 19:32:22 -0400

    separate version, init, subcommands

---
+++ pkg/userFlags/version.go
@@ -0,0 +1,13 @@
+// Package userflags have everything related to user's flags & subcommands
+package userflags
+
+import (
+	"fmt"
+)
+
+// Until I have more time to build a solid makefile, this should suffice
+const version = "v0.1.0-beta.5.2"
+
+func getVersion() {
+	fmt.Printf("version %s\n", version)
+}
