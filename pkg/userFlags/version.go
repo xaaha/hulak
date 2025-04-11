57b6b102cd8b704fee24a173ba87acd84aaf9176
xaaha <pratik.philos@yahoo.com>
2025-04-11 10:23:13 -0400

    version update

--- pkg/userFlags/version.go
+++ pkg/userFlags/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Until I have more time to build a solid makefile, this should suffice
-const version = "v0.1.0-beta.5.3"
+const version = "v0.1.0"
 
 func getVersion() {
 	fmt.Printf("%s\n", version)
