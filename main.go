1e72a85f7dede7e164c1671812b343e57594a79d
Pratik Thapa <pratikthapa@pratiks-mbp.mynetworksettings.com>
2026-01-31 11:58:45 -0500

    add todo-back

--- main.go
+++ main.go
@@ -48,6 +48,7 @@ func main() {
 	if hasFileFlags || hasDirFlags {
 		HandleAPIRequests(envMap, debug, filePathList, dir, dirseq, fp)
 	} else {
+		// TODO: Add case to handle `hulak -env prod gql .`
 		utils.PrintWarning("No file or directory specified. Use -file, -fp, -dir, or -dirseq flags.")
 	}
 }
