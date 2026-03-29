ac1cc6922029c820fc98486afbb69c7dd046f489
Pratik Thapa <pratikthapa@pratiks-mbp.mynetworksettings.com>
2026-03-29 15:57:38 -0400

    store identity and secret

--- pkg/vault/identity.go
+++ pkg/vault/identity.go
@@ -1,7 +1,17 @@
 package vault
 
 import (
+	"os"
+	"path/filepath"
+
 	"filippo.io/age"
+
+	"github.com/xaaha/hulak/pkg/utils"
+)
+
+const (
+	identityFile  = "identity.txt"
+	publicKeyFile = "key.pub"
 )
 
 type AgeKey struct {
@@ -21,25 +31,24 @@ func GenerateKeyPair() (AgeKey, error) {
 	return AgeKey{EncKey: publicKey, DecKey: privateKey}, nil
 }
 
-// func StoreIdentity() error {
-// 	// get the config locaiton from utils getUserConfigLocation
-// 	configPath, err := utils.UserConfigDir()
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	// generate the identity with GenerateKeyPair
-// 	ageKey, err := GenerateKeyPair()
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	// Get the hulak working dir os.gwd
-// 	cwd, err := os.Getwd()
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	// Save the private key in the config location with proper filePerm
-// 	// save the public key in the cwd/.hulak/key.pub
-// }
+// StoreIdentity writes the private key to the global config identity file.
+func StoreIdentity(privateKey string) error {
+	configDir, err := utils.UserConfigDir()
+	if err != nil {
+		return err
+	}
+
+	privKeyPath := filepath.Join(configDir, identityFile)
+	return os.WriteFile(privKeyPath, []byte(privateKey+"\n"), utils.SecretPer)
+}
+
+// StorePublicKey writes the public key to .hulak/key.pub in the project root.
+func StorePublicKey(publicKey string) error {
+	markerPath, err := utils.GetProjectMarker()
+	if err != nil {
+		return err
+	}
+
+	pubKeyPath := filepath.Join(markerPath, publicKeyFile)
+	return os.WriteFile(pubKeyPath, []byte(publicKey+"\n"), utils.FilePer)
+}
