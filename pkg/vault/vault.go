c51ed04a373a50ab1f6da390de25e8aace78ae79
Pratik Thapa <pratikthapa@pratiks-mbp.mynetworksettings.com>
2026-03-28 23:11:45 -0400

    Encrypt

--- pkg/vault/vault.go
+++ pkg/vault/vault.go
@@ -1,12 +1,18 @@
 package vault
 
 import (
+	"bytes"
+
 	"filippo.io/age"
 )
 
 type AgeKey struct {
 	EncKey string
 	DecKey string
+}
+
+type AgeRS struct {
+	Receipient age.Recipient
 }
 
 func GenerateKeyPair() (AgeKey, error) {
@@ -19,4 +25,25 @@ func GenerateKeyPair() (AgeKey, error) {
 	privateKey := id.String()
 
 	return AgeKey{EncKey: publicKey, DecKey: privateKey}, nil
+}
+
+func Encrypt(plaintext []byte, recepients []age.Recipient) ([]byte, error) {
+	var buf bytes.Buffer
+
+	wc, err := age.Encrypt(&buf, recepients...)
+	if err != nil {
+		return nil, err
+	}
+
+	// feed plaintext to encryptor
+	if _, err = wc.Write(plaintext); err != nil {
+		return nil, err
+	}
+
+	err = wc.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
