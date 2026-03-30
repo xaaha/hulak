4fbea98a59eb1314ac28c95b47063f350c87a8b5
Pratik Thapa <pratikthapa@pratiks-mbp.mynetworksettings.com>
2026-03-29 23:20:13 -0400

    delete identity and small refactor

--- pkg/vault/identity.go
+++ pkg/vault/identity.go
@@ -31,15 +31,23 @@ func GenerateKeyPair() (AgeKey, error) {
 	return AgeKey{EncKey: publicKey, DecKey: privateKey}, nil
 }
 
+func getIdentityFile() (string, error) {
+	configDir, err := utils.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+	privKeyPath := filepath.Join(configDir, identityFile)
+	return privKeyPath, nil
+}
+
 // StoreIdentity writes the private key to the global config identity file.
 func StoreIdentity(privateKey string) error {
-	configDir, err := utils.UserConfigDir()
+	identityFilePath, err := getIdentityFile()
 	if err != nil {
 		return err
 	}
 
-	privKeyPath := filepath.Join(configDir, identityFile)
-	return os.WriteFile(privKeyPath, []byte(privateKey+"\n"), utils.SecretPer)
+	return os.WriteFile(identityFilePath, []byte(privateKey+"\n"), utils.SecretPer)
 }
 
 // StorePublicKey writes the public key to .hulak/key.pub in the project root.
@@ -51,4 +59,16 @@ func StorePublicKey(publicKey string) error {
 
 	pubKeyPath := filepath.Join(markerPath, publicKeyFile)
 	return os.WriteFile(pubKeyPath, []byte(publicKey+"\n"), utils.FilePer)
+}
+
+func DeleteIdentity() error {
+	identityFilePath, err := getIdentityFile()
+	if err != nil {
+		return err
+	}
+	err = os.Remove(identityFilePath)
+	if err != nil {
+		return err
+	}
+	return nil
 }
