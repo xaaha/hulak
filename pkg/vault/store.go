7220af4e6d435ae8844fff668d03fe0545c120a3
Pratik Thapa <pratikthapa@pratiks-mbp.mynetworksettings.com>
2026-03-31 14:10:08 -0400

    set and delete

--- pkg/vault/store.go
+++ pkg/vault/store.go
@@ -29,3 +29,25 @@ func (s *Store) ListEnvs() []string {
 	sort.Strings(envs)
 	return envs
 }
+
+// SetKey upserts a key-value pair in the given environment.
+// Creates the environment if it does not exist.
+func (s *Store) SetKey(envName, key string, value any) {
+	if s.Envs == nil {
+		s.Envs = make(map[string]Env)
+	}
+	if s.Envs[envName] == nil {
+		s.Envs[envName] = make(Env)
+	}
+	s.Envs[envName][key] = value
+}
+
+// DeleteKey removes a key from the given environment.
+// No-op if the environment or key does not exist.
+func (s *Store) DeleteKey(envName, key string) {
+	env := s.Envs[envName]
+	if env == nil {
+		return
+	}
+	delete(env, key)
+}
