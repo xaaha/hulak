a7781915281d0f22ec5f545637442b0be01d0f85
Pratik Thapa <pratikthapa@pratiks-mbp.mynetworksettings.com>
2026-03-31 12:52:38 -0400

    reset

--- pkg/vault/identity.go
+++ pkg/vault/identity.go
@@ -4,8 +4,6 @@ import (
 	"os"
 	"path/filepath"
 
-	"filippo.io/age"
-
 	"github.com/xaaha/hulak/pkg/utils"
 )
 
@@ -13,23 +11,6 @@ const (
 	identityFile  = "identity.txt"
 	publicKeyFile = "key.pub"
 )
-
-type AgeKey struct {
-	EncKey string
-	DecKey string
-}
-
-func GenerateKeyPair() (AgeKey, error) {
-	id, err := age.GenerateX25519Identity()
-	if err != nil {
-		return AgeKey{EncKey: "", DecKey: ""}, err
-	}
-
-	publicKey := id.Recipient().String()
-	privateKey := id.String()
-
-	return AgeKey{EncKey: publicKey, DecKey: privateKey}, nil
-}
 
 func getIdentityFile() (string, error) {
 	configDir, err := utils.UserConfigDir()
@@ -40,8 +21,20 @@ func getIdentityFile() (string, error) {
 	return privKeyPath, nil
 }
 
-// StoreIdentity writes the private key to the global config identity file.
-func StoreIdentity(privateKey string) error {
+func GetIdentity() (string, error) {
+	path, err := getIdentityFile()
+	if err != nil {
+		return "", err
+	}
+	byt, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(byt), nil
+}
+
+// SetIdentity writes the private key to the global config identity file.
+func SetIdentity(privateKey string) error {
 	identityFilePath, err := getIdentityFile()
 	if err != nil {
 		return err
@@ -60,15 +53,4 @@ func DeleteIdentity() error {
 		return err
 	}
 	return nil
-}
-
-// StorePublicKey writes the public key to .hulak/key.pub in the project root.
-func StorePublicKey(publicKey string) error {
-	markerPath, err := utils.GetProjectMarker()
-	if err != nil {
-		return err
-	}
-
-	pubKeyPath := filepath.Join(markerPath, publicKeyFile)
-	return os.WriteFile(pubKeyPath, []byte(publicKey+"\n"), utils.FilePer)
 }
