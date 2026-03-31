89c7eee9880af5c17ee086ba28d812c0157236c6
Pratik Thapa <pratikthapa@pratiks-mbp.mynetworksettings.com>
2026-03-31 13:19:48 -0400

    enc key

--- pkg/vault/store.go
+++ pkg/vault/store.go
@@ -7,13 +7,13 @@ import (
 	"github.com/xaaha/hulak/pkg/utils"
 )
 
-// SetPublicKey writes the public key to .hulak/key.pub in the project root.
-func SetPublicKey(publicKey string) error {
+// SetPublicKey writes the public encryption key to .hulak/key.pub in the project root.
+func SetPublicKey(publicEncKey string) error {
 	markerPath, err := utils.GetProjectMarker()
 	if err != nil {
 		return err
 	}
 
 	pubKeyPath := filepath.Join(markerPath, publicKeyFile)
-	return os.WriteFile(pubKeyPath, []byte(publicKey), utils.FilePer)
+	return os.WriteFile(pubKeyPath, []byte(publicEncKey), utils.FilePer)
 }
